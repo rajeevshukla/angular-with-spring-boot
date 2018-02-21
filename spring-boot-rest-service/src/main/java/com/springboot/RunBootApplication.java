a68bd0ffb435e567d1ad7289bfb5da4e5b22a8af
rajeevshukla <mail2rajeevshukla@gmail.com>
2018-02-20 20:15:00 -0500

    Adding login check with json.

--- spring-boot-rest-service/src/main/java/com/springboot/RunBootApplication.java
+++ spring-boot-rest-service/src/main/java/com/springboot/RunBootApplication.java
@@ -2,14 +2,11 @@ package com.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.springboot")
 public class RunBootApplication {
 
 	public static void main(String[] args) {
-		
 		SpringApplication.run(RunBootApplication.class, args);
 	}
 
--- spring-boot-rest-service/src/main/java/com/springboot/controller/LoginController.java
+++ spring-boot-rest-service/src/main/java/com/springboot/controller/LoginController.java
@@ -5,25 +5,31 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.exception.AuthenticationException;
 import com.springboot.model.Login;
+import com.springboot.model.LoginStatus;
 import com.springboot.service.LoginService;
 
 @RestController
 public class LoginController {
-	
+
 	@Autowired
 	private LoginService loginService;
-	
-	@PostMapping("/login")
-	public Login submitLogin(@ModelAttribute Login login) {
 
+	@PostMapping("/login")
+	public LoginStatus submitLogin(@ModelAttribute Login login) {
+		LoginStatus loginStatus = new LoginStatus();
 		System.out.println(login);
-		 
-		if(loginService.validateCredentials(login))
-		    login.setPassword("made call to service");
-		 else 
-		   	login.setPassword("there is some problem");
-		return login;
-	} 
-	
+
+		try {
+			if (loginService.validateCredentials(login))
+				loginStatus.setStatus(true);
+		} catch (AuthenticationException e) {
+			e.printStackTrace();
+			loginStatus.setErrorMsg(e.getMessage());
+		}
+
+		return loginStatus;
+	}
+
 }
