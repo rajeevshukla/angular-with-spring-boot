5267562d70bc09af49531e74de8ee5e645ea7e26
rajeevshukla <mail2rajeevshukla@gmail.com>
2018-03-27 13:59:39 -0400

    Adding list of components

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
+import { UpdateProfileComponent } from './update-profile/update-profile.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponentComponent,
+    HomeComponent,
+    RegisterUserComponent,
+    UpdateProfileComponent
   ],
   imports: [
     BrowserModule,
