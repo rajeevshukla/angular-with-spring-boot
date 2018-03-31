3aa35073bb8dd810886282888b05971e741d0a58
rajeevshukla <mail2rajeevshukla@gmail.com>
2018-03-31 16:59:37 -0400

    Adding LocalContextHolder for avoiding mentioning this in local context paramter.

--- spring-boot-rest-service/src/main/java/com/springboot/RunBootApplication.java
+++ spring-boot-rest-service/src/main/java/com/springboot/RunBootApplication.java
@@ -9,10 +9,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.data.rest.core.mapping.RepositoryResourceMappings;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
@@ -37,13 +40,25 @@ public class RunBootApplication extends WebMvcConfigurerAdapter {
 	}
     
 	
-	
+	/**
+	 * Be careful about the method name it should not be getter or setting just localResolver
+	 * */
 	@Bean
-	public LocaleResolver getLocalResolver() { 
-		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+	public LocaleResolver localResolver() { 
+		/*SessionLocaleResolver localeResolver = new SessionLocaleResolver();*/
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
 		localeResolver.setDefaultLocale(Locale.US);
 		 return localeResolver;
 	}
 	
+	
+	/*Be careful about the method name it should be messageSource incase you want to load 
+	local message.properties*/
+	@Bean 
+	public ResourceBundleMessageSource messageSource() { 
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasename("message");
+		return source;
+	}
 	
 }
--- spring-boot-rest-service/src/main/java/com/springboot/controller/SignUpController.java
+++ spring-boot-rest-service/src/main/java/com/springboot/controller/SignUpController.java
@@ -6,20 +6,30 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.springboot.model.User;
 
 @RestController
+@RequestMapping("/v1")
 public class SignUpController {
+	
+	@Autowired
+	MessageSource source;
 
 	public ResponseEntity<String> signup(@RequestBody User user) {
 		 //service.save()
@@ -57,5 +67,19 @@ public class SignUpController {
 		
 		return resource;
 	}
+	
+	
+	@GetMapping("/welcome")
+	public String welcome(/*@RequestHeader(name="Accept-Language", required= false) Locale locale*/) {
+		// null == method takes multiple arguments which means you can define variable in prop file 
+		// and then you can pass those values here it will be parsed automatically.  
+		//return source.getMessage("welcome.message",null, locale);
+		
+		// to avoid writing method parameter notion you can use LocalContextHolder.
+		return  source.getMessage("welcome.message",null,LocaleContextHolder.getLocale()); 
+		
+		
+	 }
+	
 	
 }
